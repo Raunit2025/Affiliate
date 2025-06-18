1f2237514d3ab26ce36835ff1a36c363ced34638
Raunit2025 <raunitraj2336@gmail.com>
2025-06-18 16:01:32 +0530

    //

--- mern_project_1/src/LoginForm.jsx
+++ mern_project_1/src/LoginForm.jsx
@@ -36,7 +36,7 @@ const Login = () => {
               value={form.username}
               onChange={handleChange}
               className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="yourUserName"
+              placeholder="UserName"
               required
             />
           </div>
