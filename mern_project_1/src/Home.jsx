aff0fa6fb548688331f7d49a09815ef8d52add1d
Raunit2025 <raunitraj2336@gmail.com>
2025-06-19 15:31:51 +0530

    serverCreated

--- mern_project_1/src/Home.jsx
+++ mern_project_1/src/Home.jsx
@@ -5,7 +5,6 @@ function Home() {
         <div className="min-h-screen flex items-center justify-center flex-col bg-radial-[at_25%_25%] from-white to-zinc-900 to-75%">
             <h1 className="text-3xl font-bold mb-4">Welcome to MERN Project</h1>
             <Link to="/login" className="hover:text-blue-500 text-2xl ml-4 ">Login</Link>
-            <Link to="/register" className="hover:text-blue-500 text-2xl ml-4 ">Register</Link>
         </div>
     );
 }
