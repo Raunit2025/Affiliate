6db8ccd40305ba5c2051f3db4526225a33869bea
Raunit2025 <raunitraj2336@gmail.com>
2025-06-20 14:11:54 +0530

    ServerRepoChanged

--- mern-project-server/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require('../controller/authController');
-const router = express.Router(); // Instance of Router
-
-router.post('/login', authController.login);
-router.post('/login', authController.logout);
-router.post('/login', authController.isUserLoggedIn);
-
-module.exports = router;
