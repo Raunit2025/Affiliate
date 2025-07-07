a0a677bb0a9045226a96e0d4935a23f804a81197
Raunit2025 <raunitraj2336@gmail.com>
2025-07-07 15:28:06 +0530

    todaysChanges

--- mern-project-server/src/controller/paymentController.js
+++ mern-project-server/src/controller/paymentController.js
@@ -1,7 +1,8 @@
 const Razorpay = require('razorpay');
-const CREDIT_PACKS = require("../constants/paymentConstants");
+const {CREDIT_PACKS, PLAN_IDS} = require("../constants/paymentConstants");
 const crypto = require('crypto');
 const Users = require('../model/Users');
+const { default: subscriptions } = require('razorpay/dist/types/subscriptions');
 
 const razorpay = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
@@ -69,6 +70,33 @@ const paymentController = {
             });
         }
     },
+
+    createSubscription: async  (request, response) => {
+        try{
+            const { plan_name } = request.body;
+            if(!PLAN_IDS[plan_name]) {
+                return response.status(400).json({
+                    message: 'Invalid plan name'
+                });
+            }
+
+            const plan = PLAN_IDS[plan_name];
+            await razorpay.subscriptions.create({
+                plan_id:  plan.id,
+                customer_notify: 1,
+                total_count: plan.totalBillingCycleCount,
+                notes: {
+                    userId: request.user.id
+                }
+            });
+            response.json({subscription: subscription})
+        } catch (error) {
+            console.log(error);
+            response.status(500).json({
+                message: 'Internal server error'
+            });
+        }
+    }
 };
 
 module.exports = paymentController;
