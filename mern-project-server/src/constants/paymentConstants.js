f2e74b9a2864aeaf383e9eb7097beb161ab489d5
Raunit Raj <103110498+Raunit2025@users.noreply.github.com>
2025-09-14 23:21:44 +0530

    Update paymentConstants.js

--- mern-project-server/src/constants/paymentConstants.js
+++ mern-project-server/src/constants/paymentConstants.js
@@ -1,4 +1,3 @@
-// mern-project-server/src/constants/paymentConstants.js
 const CREDIT_PACKS = {
     10: 10,
     20: 20,
@@ -10,12 +9,12 @@ const PLAN_IDS = {
     UNLIMITED_YEARLY: {
         id: process.env.RAZORPAY_YEARLY_PLAN_ID,
         planName: 'Unlimited Yearly',
-        totalBillingCycleCount: 30 // MODIFIED: Set to 30 years worth of cycles
+        totalBillingCycleCount: 30 
     },
     UNLIMITED_MONTHLY: {
         id: process.env.RAZORPAY_MONTHLY_PLAN_ID,
         planName: 'Unlimited Monthly',
-        totalBillingCycleCount: 360 // MODIFIED: Set to 30 years worth of cycles
+        totalBillingCycleCount: 360 
     },
 };
 
